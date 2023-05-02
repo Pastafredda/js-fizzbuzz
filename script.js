89f6e599d2fe13529f15d99688b4fb90eb1e4ebe
Pastafredda <justspotify09@gmail.com>
2023-05-02 23:09:33 +0200

    bonus 1

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const contenitore= document.getElementById("container")
+console.log(contenitore);
+
 // Dichiariamo i numeri da 0 a 100
 for(let i = 1; i <= 100; i++){
-    
+
     // Poniamo la condizione che sia i multipli di 3 che di 5 stampi fizzbuzz
     if (i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
@@ -21,5 +24,19 @@ for(let i = 1; i <= 100; i++){
     else{
         console.log(i);
     }
+
+    // aggiungiamo uno span 
+    const quadrato= document.createElement('span')
+    
+    // diamo una classe ad ogni span
+    quadrato.classList.add("square")
+    
+    // Inseriamo i numeri nella classe
+    quadrato.append(i)
+    
+    // aggiungiamo lo span con le sue classi e il contenuto nel nostro contenitore
+    contenitore.append(quadrato)
 }
+
+
 
